7426a6f885a9bc316a87d25f5e9978f2dc26aa52
cecilia orlando <mariaceciliaorlando2021@gmail.com>
2025-01-30 05:11:21 -0300

    primer commit

---
+++ app/page.js
@@ -0,0 +1,16 @@
+import Card from '@/components/Cards';
+import Image from 'next/image';
+
+export default async function Home() {
+    const response = await fetch('https://rickandmortyapi.com/api/character');
+    const { results } = await response.json();
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+                {results.map((character) => (
+                    <Card key={character.id} character={character} />
+                ))}
+            </div>
+        </main>
+    );
+}
